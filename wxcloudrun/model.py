bd51574d5287bdcd366d3f41153d151b484fa8e7
liuw <aptx@vip.163.com>
2025-09-17 00:33:24 +0800

    enhance the time columns

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from wxcloudrun import db
+from sqlalchemy.sql import func
 
 
 # 计数表
@@ -11,8 +12,8 @@ class Counters(db.Model):
     # 设定结构体对应表格的字段
     id = db.Column(db.Integer, primary_key=True)
     count = db.Column(db.Integer, default=1)
-    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
-    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=func.now())
+    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now())
 
 
 # 封面图片表
@@ -23,8 +24,8 @@ class CoverPicture(db.Model):
     picture_name = db.Column(db.String(255), nullable=False, unique=True)
     file_url = db.Column(db.String(500), nullable=False)
     primary_cover = db.Column(db.Boolean, default=False)
-    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
-    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=func.now())
+    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now())
 
 
 # 用户表
@@ -37,5 +38,5 @@ class User(db.Model):
     comment = db.Column(db.Text)
     role = db.Column(db.Enum('ADMIN', 'VIP', 'GUEST'), default='GUEST')
     extra_message = db.Column(db.Text)
-    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
-    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=func.now())
+    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=func.now(), onupdate=func.now())
