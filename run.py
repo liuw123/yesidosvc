7f68877e36cbba8443c604cc1fbfd9ad440a0906
liuw <aptx@vip.163.com>
2025-09-17 00:57:17 +0800

    remove temp logic

--- run.py
+++ run.py
@@ -2,9 +2,7 @@
 import sys
 
 from wxcloudrun import app
-from init_db import init_database
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    init_database()
     app.run(host=sys.argv[1], port=sys.argv[2])
