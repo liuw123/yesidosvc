36962f3c1fb2ed05cec8a8cf529cdfa3773d2200
liuw <aptx@vip.163.com>
2025-09-16 23:34:57 +0800

    rename cos bucket

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
 COS_SECRET_ID = os.environ.get("COS_SECRET_ID", "your_secret_id")
 COS_SECRET_KEY = os.environ.get("COS_SECRET_KEY", "your_secret_key")
 COS_REGION = os.environ.get("COS_REGION", "ap-beijing")
-COS_BUCKET_NAME = os.environ.get("COS_BUCKET_NAME", "wechat-miniprogram")
+COS_BUCKET_NAME = os.environ.get("COS_BUCKET", "wechat-miniprogram")
 
 # 管理员密钥
 ADMIN_SECRET = os.environ.get("ADMIN_SECRET", "admin_secret_key_2024")
